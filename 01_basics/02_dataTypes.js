eace4d1f7808ca60085492bbaadf0eedea718bbf
Shailesh pandey <105715056+Pandey-Shailesh@users.noreply.github.com>
2023-12-08 07:33:58 +0000

    dataTypes is done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //we are using node.js ,not browser
+console.log(3
+    +3);   //code readability should be high
+
+ console.log("Shailesh");
+
+
+ let name="Shailesh"  //String
+ let age =18           //number
+ let isLoggedIn = false //boolean
+ let state = null
+
+//number =>2 to power 53
+//bigint
+//string=""
+//boolean
+//null => stand alone value
+// undefined =>not assign value
+//symbol => unique 
+
+//object
+
+console.log(typeof state);
+console.log(typeof undefined);
+console.log(typeof age);
+console.log(typeof "Shailesh");
