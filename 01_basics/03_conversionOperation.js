9cba0463a274fe4828bce2e448fb42ecbf8135b4
Shailesh pandey <105715056+Pandey-Shailesh@users.noreply.github.com>
2023-12-08 08:28:48 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "true"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+//NaN => Not a Number
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0
+//null=>0
+//undefined=>NaN
+//"shailesh"=>NaN
+
+let isLoggedIn=1
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+//1=>true;0=>false
+//""=>false
+//"shailesh"=true
+
+
+let someNumber =33
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
