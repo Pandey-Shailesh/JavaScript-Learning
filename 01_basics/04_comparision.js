15d4863c49c0a399be9cce08758e24bfcd4bff69
Shailesh pandey <105715056+Pandey-Shailesh@users.noreply.github.com>
2023-12-11 07:09:33 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2==1);
+console.log(2!=1);
+
+console.log("2">1);
+console.log("02">1);
+
+
+
+//We generally avoide these type of comparision
+console.log(null==0);
+console.log(null>0);
+console.log(null>=0);
+
+console.log(undefined ==0);
+console.log(undefined >=0);
+console.log(undefined <0);
+
+//strick check (===)
+
+console.log("2"===2);
