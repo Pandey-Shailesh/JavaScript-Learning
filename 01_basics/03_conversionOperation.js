be562e0b5d4ed66a678311c3dad225320be11e96
Shailesh pandey <105715056+Pandey-Shailesh@users.noreply.github.com>
2023-12-11 06:53:53 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -31,3 +31,44 @@ let stringNumber= String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+
+
+
+//***********Operations***************
+
+let value= 3
+let negvalue =-value
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);
+
+let str1= "hello"
+let str2=" Shailesh"
+let str3 =str1+str2
+console.log(str3);
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3= 3+3
+
+
+let gameCounter=100
+gameCounter++    //postfix operator
+console.log(gameCounter);
+++gameCounter    //prefix operator
+console.log(gameCounter);
