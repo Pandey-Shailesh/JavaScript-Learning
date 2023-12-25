5aac563d5868452662629d055d27763bde2a50a4
Shailesh pandey <105715056+Pandey-Shailesh@users.noreply.github.com>
2023-12-25 06:49:01 +0000

    method of String

---
+++ 01_basics/05_string.js
@@ -0,0 +1,107 @@
+// const name ="Shailesh ";
+// const repoCount = 50
+
+
+// console.log(name + repoCount);
+
+// console.log(`hello , my name is ${ name} and my repo Count is ${repoCount}`);
+// const gameName= new String("Shailesh");
+
+// console.log(gameName);
+// console.log(gameName[1]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('a'));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+// const newStringOne = "   hitesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://shailesh.com/shailesh%20pandey"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('sundar'))
+
+// console.log(gameName.split('-'));
+
+//***************************Methods of String**************************************** */
+
+
+var fname= new String("Vinay");
+
+//1.length
+console.log(fname.length);
+
+//2.anchor
+//Deprecated: This feature is no longer recommended.
+
+//3.at(index)
+console.log(fname.at(4));
+//it gives the character at index
+
+//4.big()
+//5.blink()
+//6.bold()
+// all the 3 methods are Deprecated: This feature is no longer recommended.
+
+//7. charAt(index)
+console.log(fname.charAt(3));
+//Returns the character at the specified index.
+
+//8.charCodeAt(index)
+console.log(fname.charCodeAt(3));
+//Returns the Unicode value of the character at the specified location.
+
+//9. codePointAt(index)
+console.log(fname.codePointAt(6));
+//Returns a nonnegative integer Number 
+
+//10. concat(String)
+console.log(fname.concat(" Sonkar"));
+//Returns a string that contains the concatenation of two or more strings.
+
+//11.endsWith('char')
+console.log(fname.endsWith("c"));
+//Returns boolean
+
+//12.fixed()
+//13.fontcolor()
+//14.fontsize()
+// all the 3 methods are Deprecated: This feature is no longer recommended.
+
+//15. includes(String)
+console.log(fname.includes("nay"));
+//Returns boolean
+
+//16. indexOf(char)
+console.log(fname.indexOf('V'));
+//Returns the position of the first occurrence of a substring.
+
+//17.italics()
+//18.link()
+//19. small()
+//20.strike()
+//21.sub()
+//22.substring()
+//23.sup()
+// all the 7 methods are Deprecated: This feature is no longer recommended.
+
+
+//24. lastIndexOf()
+console.log(fname.lastIndexOf("ay"));
+//Returns the last occurrence of a substring in the string.
+
+//25.match()
